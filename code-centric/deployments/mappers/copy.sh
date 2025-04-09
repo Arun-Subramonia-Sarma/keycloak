bc7bf8476368b12e5becb2bf82683d08ab988ee8
Aravind Viswanathan <aravind.viswanathan@fourkites.com>
2025-04-09 14:53:02 +0530

    Built afresh

--- code-centric/deployments/mappers/copy.sh
+++ code-centric/deployments/mappers/copy.sh
@@ -1,4 +1,4 @@
-export BASE_FOLDER=/Users/arun.sarma/my_exp/db_migration/keycloak/upgrade/keycloak
+export BASE_FOLDER=~/Dev/Java/keycloak
 export DOCKER_FOLDER=$BASE_FOLDER/code-centric/deployments/Docker
 rm $DOCKER_FOLDER/*.jar
 cp $BASE_FOLDER/company-role-mapper/target/*.jar $DOCKER_FOLDER
