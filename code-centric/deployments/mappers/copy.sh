646d0b1ad5d2d0ff9c18e0c9b812cff3235e16d6
Arun Subramonia Sarma <arun.sarma@fourkites.com>
2025-04-12 14:12:33 +0530

    Added transformer

--- code-centric/deployments/mappers/copy.sh
+++ code-centric/deployments/mappers/copy.sh
@@ -1,5 +1,6 @@
 export BASE_FOLDER=../../..
 export DOCKER_FOLDER=$BASE_FOLDER/code-centric/deployments/Docker
 rm $DOCKER_FOLDER/*.jar
-cp $BASE_FOLDER/role_mapper/company-role-mapper/target/*.jar $DOCKER_FOLDER
-cp $BASE_FOLDER/role_mapper/hello-role-mapper/target/*.jar $DOCKER_FOLDER
+# cp $BASE_FOLDER/role_mapper/company-role-mapper/target/*.jar $DOCKER_FOLDER
+# cp $BASE_FOLDER/role_mapper/hello-role-mapper/target/*.jar $DOCKER_FOLDER
+cp $BASE_FOLDER/role_mapper/fk-keycloak-ext/target/*.jar $DOCKER_FOLDER
